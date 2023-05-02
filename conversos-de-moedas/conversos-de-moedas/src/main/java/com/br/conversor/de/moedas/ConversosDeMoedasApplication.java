bfd5bd749ed0d62d7b2a047cdf779643a0c6c53d
SosthenesMS <sosthenesms@yahoo.com.br>
2023-05-02 17:44:22 -0300

    Commit inicial

---
+++ conversos-de-moedas/conversos-de-moedas/src/main/java/com/br/conversor/de/moedas/ConversosDeMoedasApplication.java
@@ -0,0 +1,13 @@
+package com.br.conversor.de.moedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversosDeMoedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversosDeMoedasApplication.class, args);
+	}
+
+}
---
+++ conversos-de-moedas/conversos-de-moedas/src/test/java/com/br/conversor/de/moedas/ConversosDeMoedasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.conversor.de.moedas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversosDeMoedasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
